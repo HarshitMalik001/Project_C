9f6009e46482a411d77b596a40f92d6b5e220a39
HarshitMalik001 <122423686+HarshitMalik001@users.noreply.github.com>
2024-08-26 21:08:37 +0530

    adding welcome function

--- main/game.c
+++ main/game.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+
+int Welcome()
+{
+    int user_choice = 3;
+    while(1)
+    {
+        printf("Welcome To 3 in 1 Game !!!!! \n");
+        printf("Options :- \n");
+        printf("1. Register \n");
+        printf("2. Login \n");
+        printf("3. Exit \n");
+
+        scanf("%d", &user_choice);
+
+        switch (user_choice)
+        {
+        case 1:
+            //register();
+            break;
+        case 2:
+            //int l = login();
+            break;
+        case 3:
+            return 0;
+            break;
+        default:
+            printf("Wrong Choice !!!!!\n");
+            break;
+        }
+
+        user_choice = 3;
+    }
+}
+
 int main()
 {
+    Welcome();
     return 0;
 }
